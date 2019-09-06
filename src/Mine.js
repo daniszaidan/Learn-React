03a2dc8951b8e7e1f9ca5836c98623747ae1234e
Danis Zaidan <daniszaidan@gmail.com>
2019-09-06 11:26:34 +0700

    state

--- src/Mine.js
+++ src/Mine.js
@@ -1,8 +1,40 @@
-import React from 'react'
-import Child from './Child'
+import React from 'react';
+import Child from './Child';
 
 class Mine extends React.Component {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         text: []
+    //     }
+
+    //     console.log('constructor');
+    // }
+
+    state = {
+        text: []
+    }
+
+    componentDidMount() {
+        // console.log('did mount');
+
+        //jangan nge-set state di dalam render, set di dalam componentDidMount
+        this.setState(
+            {
+                text: [
+                    'Belajar React Js',
+                    'Asyik',
+                    'Gud Sekali'
+                ]
+            }
+        )
+    }
+
     render() {
+        // console.log('render');
+
+        // let texts = this.state.text.map((text, index) => <Child key={index} text={text} />)
+
         return (
             <div>
                 <div id="mine">
@@ -10,9 +42,27 @@ class Mine extends React.Component {
                 </div>
                 <p>loremcascasca</p>
 
-                <Child text='text 1' />
+                {/* <Child text='text 1' />
                 <Child text='text 2' />
-                <Child text='text 3' />
+                <Child text='text 3' /> */}
+
+                {/* {texts} */}
+
+                {
+                    this.state.text.map((text, index) => <Child key={index} text={text} />)
+                }
+
+                <button onClick={() => {
+                    this.setState(
+                        {
+                            text: [
+                                'Belajar Bangke',
+                                'Asyik',
+                                'Gud Sekali'
+                            ]
+                        }
+                    )
+                }}>Change</button>
             </div>
         )
     }
